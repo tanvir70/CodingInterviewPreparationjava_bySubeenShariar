0f9edf572dd69ace75fd20407eeab51923b418dd
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2022-08-25 02:14:35 +0600

    Update RotatedArraySearch.java

--- RotatedArraySearch.java
+++ RotatedArraySearch.java
@@ -1,18 +1,27 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RotatedArraySearch {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] array = {4, 5, 6, 0, 1, 2, 3};
+        System.out.print("What the length of the array : ");
+        int n = scan.nextInt();
+        
+        int[] array = new int[n];
+        System.out.println("Enter the array elements : ");
+        for (int i = 0; i < n; i++) {
+                array[i] = scan.nextInt();
+        }
+        System.out.println("Array : " + Arrays.toString(array));
         int left = 0;
-        int right = array.length - 1;
+        int right = n - 1;
         int mid = 0;
         System.out.print("Enter a Targeted key : ");
         int key = scan.nextInt();
         while (left <= right){
             mid = (left + right) / 2;
             if (array[mid] == key) {
-                System.out.println(mid);
+                System.out.println("Key is in " + mid "th index");
             }
             if (array[left] < array[mid] && key >= array[left] && key < array[mid])
                 right = mid - 1;
